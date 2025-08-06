ce434949bfcafd1dd880305f9c0ea860eb776ef5
aravind-3697 <43001827+aravind-3697@users.noreply.github.com>
2025-08-06 12:46:30 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,36 +2,39 @@
 from time import sleep
 import sys
 
+def display_warning(message):
+    print(message)
+    for _ in range(6):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(1)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(1)
+
+def check_temperature_range(temperature):
+    if temperature > 102 or temperature < 95:
+        display_warning('Temperature critical!')
+        return False
+    return True
+
+def check_pulse_rate_range(pulseRate):
+    if pulseRate < 60 or pulseRate > 100:
+        display_warning('Pulse Rate is out of range!')
+        return False
+    return True
+
+def check_spo2_range(spo2):
+    if spo2 < 90:
+        display_warning('Oxygen Saturation out of range!')
+        return False
+    return True
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+    if not check_temperature_range(temperature):
+        return False
+    if not check_pulse_rate_range(pulseRate):
+        return False
+    if not check_spo2_range(spo2):
+        return False
+    return True
